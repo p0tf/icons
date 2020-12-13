5445ec5da9f72d9ff65143daf5cca956b6fe8443
Potf Man <potfman@gmail.com>
2020-12-13 17:23:22 +0900

    Add santa.

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 convert -sample 128x128 16/normal.png 128/normal-16.png
-convert -sample 128x128 16/casual.png 128/casual-16.png
 convert -sample 128x128 32/normal.png 128/normal-32.png
+convert -sample 128x128 16/casual.png 128/casual-16.png
 convert -sample 128x128 32/casual.png 128/casual-32.png
+convert -sample 128x128 16/santa.png 128/santa-16.png
+convert -sample 128x128 32/santa.png 128/santa-32.png
